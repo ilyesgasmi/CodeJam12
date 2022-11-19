2b197f74c251230892f840419a0ba7d3e021315c
ilyesgasmi <77757014+ilyesgasmi@users.noreply.github.com>
2022-11-19 15:36:45 -0500

    API image detection

--- recipe-search/src/views/AboutView.vue
+++ recipe-search/src/views/AboutView.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <div class="pl-5 pr-5">
-      <b-form-file id="file-large" accept="image/*" v-model="file1" size="lg" placeholder="Choose a file or drop it here..."></b-form-file>
+    <div v-if="!food" class="pl-5 pr-5">
+      <b-form-file id="file-large" accept=".jpg, .png" v-model="file1" size="lg" placeholder="Choose a file or drop it here..."></b-form-file>
       <div v-if="url" style="display: flex;justify-content: center;">
         <img style="box-shadow: rgba(0, 0, 0, 0.24) 0px 5px 15px;" :src="url" />
       </div>
       <div class="btn-center">
-        <button v-if="file1" class="button-30" @click="debug()">Find out more about my dish!</button>
+        <button v-if="file1" class="button-30" @click="getFoodCV(food)">Find out more about my dish!</button>
       </div>
     </div>
+    <div v-if="food"><h1>{{food}}</h1></div>
   </div>
 </template>
 
--- recipe-search/src/views/food_detection.js
+++ recipe-search/src/views/food_detection.js
@@ -6,7 +6,8 @@ export default {
             recipeData: [],
             recipeInputText: '',
             file1:null,
-            url:null
+            url:null,
+            food: null,
         }
     },
     watch:{
@@ -32,8 +33,49 @@ export default {
                   console.error(error);
               });
         },
-        debug: function() {
-            console.log("request sent with file:", this.file1.name)
+        getFoodCV: async function(food) {
+            var image = ""
+            await this.getBase64Image(this.file1).then((r) => {image=r;})
+            function escapeRegExp(str) { return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"); }
+            image = image.replace(new RegExp(escapeRegExp("data:image/")+'(.*?)'+escapeRegExp(";base64,")), '');
+            console.log("image:", image)
+            const myBody= 
+            {
+                "requests":[
+                  {
+                    "image":{
+                      "content": image
+                    },
+                    "features":[
+                      {
+                        "type":"WEB_DETECTION",
+                        "maxResults":5
+                      }
+                    ]
+                  }
+                ]
+            }
+            
+            await fetch('https://vision.googleapis.com/v1/images:annotate/?key=AIzaSyCivHbxHRi6zlAgu-ebIT7feom-dl-ThcU', {
+                method: 'POST',
+                body: JSON.stringify(myBody),
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            }).then((response) => response.json()).then((results) => {
+                this.food = results.responses[0].webDetection.webEntities.map((x) => [x.description, x.score])
+            })
+            console.log(this.food)
+            
+        },
+        getBase64Image: function(file) {
+            return new Promise(function (resolve, reject) {
+                let reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => resolve(reader.result)
+                reader.onerror = (error) => reject('Error: ', error);
+          })
         }
     },
     mounted(){

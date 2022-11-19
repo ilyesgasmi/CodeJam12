4bf835240a939ce42111e854561e5555c93076cf
ilyesgasmi <77757014+ilyesgasmi@users.noreply.github.com>
2022-11-19 12:38:06 -0500

    image preview and input

--- recipe-search/src/views/AboutView.vue
+++ recipe-search/src/views/AboutView.vue
@@ -4,12 +4,13 @@
       This is a recipe page <br> <br>
     </div>
     <div class="pl-5 pr-5">
-      <b-form-file id="file-large" v-model="file1" size="lg" placeholder="Choose a file or drop it here..."></b-form-file>
-      <div v-if="url" class="flex justify-center">
-        <img :src="url" />
+      <b-form-file id="file-large" accept="image/*" v-model="file1" size="lg" placeholder="Choose a file or drop it here..."></b-form-file>
+      <div v-if="url" style="display: flex;justify-content: center;">
+        <img style="box-shadow: rgba(0, 0, 0, 0.24) 0px 5px 15px;" :src="url" />
       </div>
     </div>
   </div>
 </template>
 
 <script src="./food_detection.js"></script>
+

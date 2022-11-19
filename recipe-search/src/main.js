d29e519be0ee2f1b736cfb8feaf6f9def6cccc6a
AriA700 <75389449+AriA700@users.noreply.github.com>
2022-11-18 23:58:31 -0500

    Base Commit

--- recipe-search/src/main.js
+++ recipe-search/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './index.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(BootstrapVue)
+app.use(IconsPlugin)
 app.mount('#app')
--- recipe-search/src/views/AboutView.vue
+++ recipe-search/src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="text-2xl">This is an about page</div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
